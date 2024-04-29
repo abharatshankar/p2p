60f5dcb4d0e7089ab9e2f4768e761c61925ae6f5
Bharat <apple@apples-MacBook-Pro.local>
2024-04-29 08:34:41 +0530

    added all bottom nav bar icons

--- lib/view/tab_bars.dart
+++ lib/view/tab_bars.dart
@@ -47,7 +47,7 @@ class _BottomMenuBarState extends State<BottomMenuBar > {
             BottomNavigationBarItem(  
               icon: Column(
                 children: [
-                  Icon(Icons.search,color: Colors.grey,),
+                  Image.asset('assets/P2P.png',width: 29,height: 29,),
                   Text('P2P',style: TextStyle(fontWeight: FontWeight.normal),),
                 ],
               ), 
@@ -55,7 +55,7 @@ class _BottomMenuBarState extends State<BottomMenuBar > {
               label: '',
               activeIcon: Column(
                 children: [
-                  Icon(Icons.search,color: AppColors.themeBlueColor,),
+                  Image.asset('assets/P2P_selected.png',width: 29,height: 29,),
                   Text('P2P',style: TextStyle(fontWeight: FontWeight.bold,color: AppColors.themeBlueColor,),)
                 ],
               )
@@ -64,46 +64,46 @@ class _BottomMenuBarState extends State<BottomMenuBar > {
             BottomNavigationBarItem(  
               icon: Column(
                 children: [
-                  Icon(Icons.home,color: Colors.grey,),
-                  Text('Orders',style: TextStyle(fontWeight: FontWeight.normal),),
+                  Image.asset('assets/ORDERS.png',width: 29,height: 29,),
+                  const Text('Orders',style: TextStyle(fontWeight: FontWeight.normal),),
                 ],
               ),  
               label: '',  
               backgroundColor: Colors.white  ,
               activeIcon: Column(
                 children: [
-                  Icon(Icons.home,color: AppColors.themeBlueColor,),
-                  Text('Orders',style: TextStyle(fontWeight: FontWeight.bold,color: AppColors.themeBlueColor,),),
+                  Image.asset('assets/ORDERS_selected.png',width: 29,height: 29,),
+                  const Text('Orders',style: TextStyle(fontWeight: FontWeight.bold,color: AppColors.themeBlueColor,),),
                 ],
               )
             ), 
             BottomNavigationBarItem(  
               icon: Column(
                 children: [
-                  Icon(Icons.home,color: Colors.grey,),
-                  Text('Ads',style: TextStyle(fontWeight: FontWeight.normal),),
+                  Image.asset('assets/ADS.png',width: 29,height: 29,),
+                  const Text('Ads',style: TextStyle(fontWeight: FontWeight.normal),),
                 ],
               ),  
               label: '',  
               backgroundColor: Colors.white  ,
               activeIcon: Column(
                 children: [
-                  Icon(Icons.home,color: AppColors.themeBlueColor,),
-                  Text('Ads',style: TextStyle(fontWeight: FontWeight.normal,color: AppColors.themeBlueColor,),),
+                  Image.asset('assets/ADS_selected.png',width: 29,height: 29,),
+                  const Text('Ads',style: TextStyle(fontWeight: FontWeight.normal,color: AppColors.themeBlueColor,),),
                 ],
               )
             ), 
             BottomNavigationBarItem(  
               icon: Column(
                 children: [
-                  Image.asset('assets/wallet_selected.png'),
-                  Text('Wallet',style: TextStyle(fontWeight: FontWeight.normal),),
+                  Image.asset('assets/wallet.png',width: 29,height: 29,),
+                  const Text('Wallet',style: TextStyle(fontWeight: FontWeight.normal),),
                 ],
               ),  
               label: '',  
               activeIcon: Column(
                 children: [
-                  Icon(Icons.person,color: AppColors.themeBlueColor,),
+                  Image.asset('assets/wallet_selected.png',width: 29,height: 29,),
                    Text('Wallet',style: TextStyle(fontWeight: FontWeight.normal,color: AppColors.themeBlueColor,),),
                 ],
               )
